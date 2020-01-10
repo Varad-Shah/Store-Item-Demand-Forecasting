eafddb6a87b547ff0aa8508af5ddc35cfe625f8f
Varad Shah <48764877+Varad-Shah@users.noreply.github.com>
2020-01-10 00:33:40 -0500

    Update Using Arima.R

--- Using Arima.R
+++ Using Arima.R
@@ -8,11 +8,25 @@ library(MLmetrics)
 library(openair)
 library(tidyverse)
 library(TSPred)
+library(astsa)
 
 ################################# FINDING METRICS OF THE MODEL #####################
 
 train<-read.csv("C:\\Users\\Admin\\Desktop\\R Programming\\project\\train.csv", stringsAsFactors = F)
 train[,1] = as.POSIXct(train[,1], format= "%Y-%m-%d")
+
+store1_item1 <-subset(train,train$store==1 & train$item==1) # consider only store 1 and item 1
+store1_item1_ts <- ts(store1_item1$sales, start = c(2013,1),frequency = 365) #convert to ts
+decompose_ts <- decompose(store1_item1_ts ) #decompose to see trend, seasonality and noise
+plot(decompose_ts)
+acf2(store1_item1_ts,max.lag = 40) # check stationarity
+diff_s1i1 <- diff(store1_item1_ts) #difference to convert to stationary
+plot.ts(diff_s1i1)
+acf2(diff_s1i1,max.lag = 40) # check stationarity
+
+#ACF suggests an MA(1) and PACF plot suggests an AR(1), but as 1st 7 lags are significant,
+residual_analysis <- sarima(store1_item1_ts,7,1,7) #residual analysis
+auto_arima <- auto.arima(store1_item1_ts) #compare with auto.arima
 
 reshaped.data1 <- reshape(train,  idvar =c('store', 'item'), direction = 'wide', timevar = 'date')
 ncol(reshaped.data1)
